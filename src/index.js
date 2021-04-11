216510e5bbbffd4583298175a64903936726b26e
JaSON <masteraccounthub@gmail.com>
2021-04-11 17:56:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+
+
+funfunction loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(res=> res.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
